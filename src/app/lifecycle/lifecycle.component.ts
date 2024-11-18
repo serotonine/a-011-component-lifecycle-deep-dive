bb03f16646f0209775467913b3fa2aa11949eadb
serotonine <ks.serotonine@gmail.com>
2024-11-18 16:32:11 +0100

    Master: Init#2.

---
+++ src/app/lifecycle/lifecycle.component.ts
@@ -0,0 +1,73 @@
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  standalone: true,
+  imports: [],
+  templateUrl: './lifecycle.component.html',
+  styleUrl: './lifecycle.component.css',
+})
+export class LifecycleComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() text?: string;
+
+  constructor() {
+    console.log('CONSTRUCTOR');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
+   /* ngDoCheck is invoked by Angular whenever Angular thinks that a UI update might be needed.
+      Avoid to use it because it will be invoked a lot. 
+      */
+   ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+// Template events (ng-content).
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+// Template events (ng-content).
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+// Template events.
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+// Template events.
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+}
